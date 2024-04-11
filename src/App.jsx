e697801b3e6fcb245295218bfb6b3b381f411f47
Jaymeen Patel <pjaymeen908@gmail.com>
2024-04-11 10:21:49 +0530

    Navbar created and responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import Navbar from "./components/navebar/navbar"
+import "./layout.scss"
+
 function App() {
   return (
-    <div><a href="/">Hello World</a></div>
+    <div className="layout">
+      <Navbar/>
+    </div>
   )
 }
 
