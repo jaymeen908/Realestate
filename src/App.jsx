d878a35ed38409ae91b95e085515e108668c0047
Jaymeen Patel <pjaymeen908@gmail.com>
2024-04-16 09:39:55 +0530

    How TO react dom works

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,46 @@ import Navbar from "./components/navebar/navbar"
 import "./layout.scss"
 import HomePage from "./routes/homePage/homePage"
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import ListPage from "./routes/listPage/listPage";
+
+
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <HomePage/>
+      ),
+    },
+    {
+      path: "/list",
+      element: <ListPage />,
+        
+    },
+
+  ]);
+
+
   return (
-    <div className="layout">
-      <div className="navbar">
+    // <div className="layout">
+    //   <div className="navbar">
 
-      <Navbar/>
-      </div>
-      <div className="content">
+    //   <Navbar/>
+    //   </div>
+    //   <div className="content">
 
-      <HomePage />
-      </div>
-    </div>
+    //   <HomePage />
+    //   </div>
+    // </div>
+    <RouterProvider router={router}/>
+
   )
 }
 
