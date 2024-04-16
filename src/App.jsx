896a9f55cc8deb8dbbd146496e8e4b73ab828797
Jaymeen Patel <pjaymeen908@gmail.com>
2024-04-16 09:11:15 +0530

    Responsiable for sm md lg screeen

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import Navbar from "./components/navebar/navbar"
 import "./layout.scss"
+import HomePage from "./routes/homePage/homePage"
 
 function App() {
   return (
     <div className="layout">
+      <div className="navbar">
+
       <Navbar/>
+      </div>
+      <div className="content">
+
+      <HomePage />
+      </div>
     </div>
   )
 }
