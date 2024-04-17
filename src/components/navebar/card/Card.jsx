10064083b1da31a146fc7f1a43ce3ac1ea8db77b
Jaymeen Patel <pjaymeen908@gmail.com>
2024-04-17 21:46:57 +0530

    added card and design to bottom part

--- src/components/navebar/card/Card.jsx
+++ src/components/navebar/card/Card.jsx
@@ -1,7 +1,44 @@
+import { Link } from 'react-router-dom';
 import './card.scss';
-function Card() {
+
+function Card({ item }) {
     return ( 
-        <div className="card">Card</div>
+        <div className="card">
+        <Link to={`/${item.id}`} className='imageContainer'>
+            <img src={item.img} alt=''></img>
+        </Link>
+        <div className='textContainer'>
+            <h2 className='title'>
+                <Link to={`/${item.id}`}>{item.title}</Link>
+            </h2>
+            <p className='address'>
+                <img src='/pin.png'></img>
+                <span>{item.address}</span>
+            </p>
+            <p className='price'>{item.price}</p>
+            <div className='bottom'>
+            <div className='features'>
+                <div className='feature'>
+                    <img src='/bed.png' alt=''></img>
+                    <span>{item.bedroom} bedroom</span>
+                </div>
+                <div className='feature'>
+                    <img src='/bath.png' alt=''></img>
+                    <span>{item.bathroom} bathroom</span>
+                </div>
+            </div>
+            <div className='icons'>
+                  <div className='icon'>
+                    <img src='/save.png'></img>
+                  </div>
+                  <div className='icon'>
+                    <img src='/chat.png'></img>
+                  </div>
+            </div>
+            </div>
+           
+        </div>
+        </div>
      );
 }
 
